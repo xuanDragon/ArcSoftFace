badf70ac67d3a68acce2aac5533f21eb5c2896a6
1037913794 <1037913794@qq.com>
2020-04-02 14:41:44 +0800

    Commit 1.1 创建新窗口

--- ArcSoftFace/Program.cs
+++ ArcSoftFace/Program.cs
@@ -16,7 +16,8 @@ namespace ArcSoftFace
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FaceForm());
+            // Application.Run(new FaceForm());
+            Application.Run(new AuthenticationForm());
         }
     }
 }
